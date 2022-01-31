cdedd08e35a5edbf8976f00841cb303d15be14f3
Arturo <arturo.gil@umh.es>
2022-01-31 13:55:57 +0100

    added a velodyne sensor to UR5 added get_laser_data

--- artelib/robot.py
+++ artelib/robot.py
@@ -149,9 +149,11 @@ class Robot():
 
     def get_laser_data(self):
         """
-        Caution: a signal must have been added to the Coppelia Simulation (called distance_to_sphere)
+        This reads the laserdata signal in Coppelia and returns it.
+        The laserdata signal must be defined as in the UR5_velodyne.ttt environment.
         """
         error, data = sim.simxGetStringSignal(self.clientID, 'laserdata', sim.simx_opmode_oneshot_wait)
+        # TODO: after unpacking the floats, some more-readable data structure should be built.
         data = sim.simxUnpackFloats(data)
         return data
 
