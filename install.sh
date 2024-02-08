3f46248dabf3979fbf392b44b17660d00ac3698f
Arturo <arturo.gil@umh.es>
2024-02-08 13:08:39 +0100

    Added new Husky Robot + Ouster OS1 sensor.

--- install.sh
+++ install.sh
@@ -1,10 +1,49 @@
 # Change INSTALLDIR to your needs. It currently points to your desktop
 # the library's directory pyARTE should be placed at the same directory.
-INSTALLDIR=`xdg-user-dir DESKTOP`
+INSTALLDIR=~/Simulations
+mkdir $INSTALLDIR
 
-# this installs the virtualenv package
+echo 'INSTALLING pyARTE in'
+echo $INSTALLDIR
+read -p "Do you want to proceed? (y/n) " choice
+case $choice in
+[yY]* ) echo "Ok, installing" ;;
+[nN]* ) exit ;;
+*) exit ;;
+esac
+
+echo 'INSTALLING virtualenv, python3-dev and git'
+read -p "Do you want to proceed? (y/n) " choice
+case $choice in
+[yY]* ) echo "Ok, installing" ;;
+[nN]* ) exit ;;
+*) exit ;;
+esac
+
+# this installs the virtualenv package and python and git
 sudo apt install virtualenv
 sudo apt install python3-dev
+sudo apt install git
+
+cd ~
+cd $INSTALLDIR
+
+echo 'CLONING repository pyARTE from github. Say no if you already cloned it'
+read -p "Do you want to proceed? (y/n) " choice
+case $choice in
+[yY]* ) git clone https://github.com/4rtur1t0/pyARTE ;;
+[nN]* ) exit ;;
+*) exit ;;
+esac
+
+echo 'NOW creating a virtual environment and installing requirements with pip in it.'
+echo $INSTALLDIR
+read -p "Do you want to proceed? (y/n) " choice
+case $choice in
+[yY]* ) git clone https://github.com/4rtur1t0/pyARTE ;;
+[nN]* ) exit ;;
+*) exit ;;
+esac
 
 # move to the home directory
 cd ~
@@ -16,5 +55,5 @@ cd venv/bin
 pwd
 echo 'INSTALLING DEPENDENCIES WITH PIP'
 ./pip install -r ../../pyARTE/requirements.txt
-cd $INSTALLDIR
+
 
