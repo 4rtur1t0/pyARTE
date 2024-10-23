15e9b184de2c3362c5895fec236ae93053089cd7
Arturo <arturo.gil@umh.es>
2024-10-23 15:51:17 +0200

    Minor refactor

---
+++ practicals/projects/year_2425_ARUCO/irb140_project_ARUCO_2425.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Please open the irb140_project_2425.ttt scene before running this script.
+
+The code beneath must be completed by the student in order to produce different assembly of the ARUCO Tetris pieces
+according to their ids.
+
+@Authors: Arturo Gil
+@Time: October 2024
+"""
+import numpy as np
+from artelib.euler import Euler
+from artelib.vector import Vector
+from artelib.homogeneousmatrix import HomogeneousMatrix
+from robots.abbirb140 import RobotABBIRB140
+from robots.grippers import SuctionPad
+from robots.objects import ReferenceFrame
+from robots.simulation import Simulation
+from robots.camera import Camera
+
+
+def pick_and_place_arucos():
+    simulation = Simulation()
+    simulation.start()
+    robot = RobotABBIRB140(simulation=simulation)
+    robot.start()
+    frame = ReferenceFrame(simulation=simulation)
+    frame.start()
+    camera = Camera(simulation=simulation, resolution=1200, fov_degrees=45)
+    camera.start(name='/IRB140/camera')
+    gripper = SuctionPad(simulation=simulation)
+    gripper.start()
+    # TCP DE LA VENTOSA! OJO!: debe ser el adecuado para la escena
+    robot.set_TCP(HomogeneousMatrix(Vector([0, 0.0, 0.05]), Euler([0, 0, 0])))
+
+    q = np.array([0, 0, 0, 0, np.pi / 2, 0])
+    robot.moveAbsJ(q)
+
+    robot.moveJ(target_position=Vector([0.4, 0.0, 0.5]),
+                target_orientation=Euler([0, np.pi, 0]))
+    robot.moveL(target_position=Vector([0.5, 0.0, 0.45]),
+                target_orientation=Euler([0, np.pi, 0]))
+    T0v = robot.directkinematics(q)
+    Tvc = HomogeneousMatrix(Vector([0, -0.05, 0.0]), Euler([0, 0, -np.pi / 2]))
+    id, Tca = camera.detect_closer_aruco(show=True, aruco_size=0.03)
+
+    T0a = T0v*Tvc*Tca
+    frame.show_target_point(target_position=T0a.pos(), target_orientation=T0a.R())
+
+    simulation.stop()
+
+
+if __name__ == "__main__":
+    pick_and_place_arucos()
+
+
