91f42e01cb584cd0280c1d74dfeea668d6553e52
Arturo <arturo.gil@umh.es>
2024-11-13 17:32:46 +0100

    Updater

--- utils/update.sh
+++ utils/update.sh
@@ -2,39 +2,24 @@
 # Change INSTALLDIR to your needs. It currently points to your desktop
 # the library's directory pyARTE should be placed at the same directory.
 INSTALLDIR=~/Escritorio/pyARTE
-#UPDATELOG=~/Escritorio/pyARTE/utils/update.log
 
 while [ "$(hostname -I)" = "" ]; do
   echo -e "\e[1A\e[KNo network: $(date)"
-#  echo -e "\e[1A\e[KNo network: $(date)" >> $UPDATELOG
   sleep 1
 done
 
 echo "I have network"
-#echo "I have network" >> $UPDATELOG
-#echo "Internet is reachable"  >> $UPDATELOG
+echo "Internet is reachable"
 
 # none reachable
 echo "Internet is up"
 echo "Updating repos"
-#echo "Internet is up updating repos" >> $UPDATELOG
 
-#sudo sudo -i -u usuario
 # update repos
 cd $INSTALLDIR
-#git config --global --add safe.directory /home/usuario/Escritorio/pyARTE/
-git stash #>> $UPDATELOG 2>&1
-git stash clear #>> $UPDATELOG 2>&1
-git pull #>> $UPDATELOG 2>&1
+git stash
+git stash clear
+git pull
 
-#cd /home/usuario/Escritorio/ARTE/
-##git config --global --add safe.directory /home/usuario/Escritorio/ARTE/
-#git stash >> $UPDATELOG 2>&1
-#git stash clear >> $UPDATELOG 2>&1
-#git pull >> $UPDATELOG 2>&1
-
-#chown -R usuario:usuario /home/usuario/Escritorio/pyARTE/
-#chown -R usuario:usuario /home/usuario/Escritorio/ARTE/
-
-echo "Process ended correctly" #>> $UPDATELOG
+echo "Process ended correctly on
 date
--- utils/update_computers.sh
+++ utils/update_computers.sh
@@ -4,16 +4,26 @@
 # Instrucciones:
 # Ordenador profesor: >> sudo apt install sshpass
 # Todos ordenadores instalar: >> sudo apt install openssh-server -y
+# Final: ejecutar este script ./update_computers.sh
 
-# Final: ejecutar este script ./update_devices.sh
+UPDATEFILE=/home/usuario/pyARTE/utils/update.sh
+LOGFILE=/home/usuario/pyARTE/utils/update.log
+
+croncmd="$UPDATEFILE > $UPDATELOG"
+cronjob="@reboot $croncmd"
+#To add it to the crontab, with no duplication:
+#( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+#To remove it from the crontab whatever its current schedule:
+#( crontab -l | grep -v -F "$croncmd" ) | crontab -
+
 
 #!/bin/bash
 for i in {2..100}
 	do
 	if ping -c1 172.16.28.$i 2>&1 2>/dev/null; then
 		echo "Found up IP: 172.16.28.$i"
-		sshpass -pusuario scp rc.local usuario@172.16.28.$i:/home/usuario/rc.local
-		sshpass -pusuario ssh usuario@172.16.28.$i "echo 'usuario' | sudo -S -k mv /home/usuario/rc.local /etc/rc.local"
+		sshpass -pusuario scp update.sh usuario@172.16.28.$i:$INSTALLFILE
+#		sshpass -pusuario ssh usuario@172.16.28.$i "echo 'usuario' | sudo -S -k mv /home/usuario/rc.local /etc/rc.local"
 		echo "SUCCESSSSSSSSSSSSSSSSSSSS ************************************"
 	else
 		echo "Could not connect to 172.16.28.$i"
